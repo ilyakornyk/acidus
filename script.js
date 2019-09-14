942d4f9f64c5db81287ebf0d40677ce8fa891c48
ilyakornyk <krekerbrow@gmail.com>
2019-09-14 18:19:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// prompt("How are you?","");
+
+// confirm("You americanets?");
+
+
+// var a = confirm("You hudoi?");
+// 	if(a = true){
+// 		alert("kapets ti loh");
+// 	}else if(a = false){
+// 		alert("okay, poidem dalishe");
+// 		var b = confirm("you visokiy?");
+
+// 		if(b = false){
+// 			alert("shut up, karlito");
+// 		}else{
+// 			alert("okay, prohodite");
+// 			var c = prompt("Kak pohodka?");
+
+// 			if(c = "normul"){
+// 				alert("V jope kurkyl");
+// 			}else if(c = "ploha"){
+
+// 				alert("pon, pandon")
+
+// 			}
+// 		}
+// 	}
+
+// alert("sanya hui sosi");
+
+ //  function a(b){
+
+ // 	var a = confirm("You school man?");
+
+ // 	if(a = true){
+ // 		alert("shut up, scholnik");
+ // 	}else if(a = false){
+ // 		alert("okay, lets hou");
+ // 		var c = confirm("A you never gona give up?");
+ // 		if(c = true){
+ // 			alert("all right");
+ // 		}else{
+ // 			alert("fuck off");
+ // 		}
+ // 	}
+ // }
+
+ // a();
